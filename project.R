482a45d622265b80ab32d8652ba38f72556b4b9e
Jack Zhang <jackzhang@Jacks-MacBook-Pro.local>
2015-03-25 22:26:15 -0400

    read sms

--- project.R
+++ project.R
@@ -6,7 +6,6 @@
 rm(list=ls(all=T))
 library(RTextTools)
 #Read SMS Spam File into table
-sms.dir <- "./data/smsspamcollection//SMSSpamCollection"
-smsFile <- file(sms.dir,open="rt",encoding="latin1")
-text <- readLines(smsFile)
-close(smsFile)
+sms.dir <- "./data/smsspamcollection/SMSSpamCollection"
+sms.data <- read.table(sms.dir,sep="\t",quote="",fill=FALSE,col.names=c("class", "sms"),)
+
