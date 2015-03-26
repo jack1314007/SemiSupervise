70acfacb4263aa16432cc592f60ff5b2a64e923c
Jack Zhang <jackzhang@Jacks-MacBook-Pro.local>
2015-03-25 20:55:40 -0400

    Initialized

--- project.R
+++ project.R
@@ -5,12 +5,8 @@
 ##############################################################
 rm(list=ls(all=T))
 library(RTextTools)
-
-get.msg <- function(path.dir)
-{
-  con <- file(path.dir, open="rt", encoding="latin1")
-  text <- readLines(con)
-  msg <- text[seq(which(text=="")[1]+1,length(text),1)]
-  close(con)
-  return(paste(msg, collapse="\n"))
-}
+#Read SMS Spam File into table
+sms.dir <- "./data/smsspamcollection//SMSSpamCollection"
+smsFile <- file(sms.dir,open="rt",encoding="latin1")
+text <- readLines(smsFile)
+close(smsFile)
