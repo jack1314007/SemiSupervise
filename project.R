5a9fc2ae02aa801d87b6b7e4a1429c16578f2eba
Jack Zhang <jackzhang@Jacks-MacBook-Pro-2.local>
2015-04-10 11:34:11 -0400

    SPA Model

--- project.R
+++ project.R
@@ -4,13 +4,47 @@
 # Author: 
 ##############################################################
 rm(list=ls(all=T))
-library(RTextTools)
+library(spa)
 #Read SMS Spam File into table
-sms.dir <- "./data/smsspamcollection/SMSSpamCollection"
-sms.data <- read.table(sms.dir,sep="\t",quote="",fill=FALSE,col.names=c("class", "sms"),)
-matrix <- create_matrix(cbind(sms.data$class,sms.data$sms),)
-con <- create_container(matrix, sms.data$class, trainSize=1:2500, testSize=2501:4000, virgin=FALSE)
-models <- train_models(con, algorithms=c("MAXENT","SVM"))
-results <- classify_models(con, models)
-analytics <- create_analytics(con, results)
-summary(analytics)
+spam <- read.table("data/spambase/spambase.data",sep=",")
+
+#sample labeled data
+label.row <- sample(1:nrow(spam),50)
+label.data <- spam[label.row,]
+
+#Unlabeled data
+unlabel.data <- spam[-label.row,]
+
+#Sample from unlabeled data to train
+unlabel.train.row <- sample(as.numeric(rownames(unlabel.data)),750)
+unlabel.train.data <- spam[unlabel.train.row,]
+unlabel.train.data[,58]<-NA
+
+#Test Cases
+testset_withTrain <- spam[-label.row,]
+testset_withoutTrain <- spam[-c(label.row,unlabel.train.row),]
+
+#Train Cases
+mixdata <- rbind(label.data,unlabel.train.data)
+mixdata.class <- mixdata[,58]
+mixdata <- mixdata[,1:54]
+train.graph <- as.matrix(daisy(mixdata))
+
+#SPA Model
+gsemi<-spa(mixdata.class,graph=train.graph,control=spa.control(gcv="aGCV"))
+
+#Predict with testsets
+
+
+train.class <- label.data[,58]
+label.data <- label.data[,1:54]
+n <- 1:nrow(label.data)
+train.graph1 <- matrix(0,nrow=nrow(label.data),ncol=nrow(label.data))
+
+for(i in n)
+{
+  for(j in n)
+  {
+    train.graph1[i,j] = sum(apply(rbind(label.data[i,],label.data[j,]),2,min))
+  }
+}
