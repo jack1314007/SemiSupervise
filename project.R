2e8ae68912865fff0fa8ae33510a2a32848d23b0
Jack Zhang <jackzhang@Jacks-MacBook-Pro-2.local>
2015-04-11 16:07:48 -0400

    update

--- project.R
+++ project.R
@@ -5,46 +5,52 @@
 ##############################################################
 rm(list=ls(all=T))
 library(spa)
+library(mda)
+
 #Read SMS Spam File into table
 spam <- read.table("data/spambase/spambase.data",sep=",")
 
-#sample labeled data
-label.row <- sample(1:nrow(spam),50)
-label.data <- spam[label.row,]
+#sample 1000 from sample pool
+sampleSet <- spam[sample(1:nrow(spam),1500),]
 
-#Unlabeled data
-unlabel.data <- spam[-label.row,]
+#Select the First 150 as FIXED test data
+testSet <- sampleSet[1351:1500,]
+#The rest data as TrainData
+trainSet <- sampleSet[1:1350,]
 
-#Sample from unlabeled data to train
-unlabel.train.row <- sample(as.numeric(rownames(unlabel.data)),750)
-unlabel.train.data <- spam[unlabel.train.row,]
-unlabel.train.data[,58]<-NA
-
-#Test Cases
-testset_withTrain <- spam[-label.row,]
-testset_withoutTrain <- spam[-c(label.row,unlabel.train.row),]
-
-#Train Cases
-mixdata <- rbind(label.data,unlabel.train.data)
-mixdata.class <- mixdata[,58]
-mixdata <- mixdata[,1:54]
-train.graph <- as.matrix(daisy(mixdata))
-
-#SPA Model
-gsemi<-spa(mixdata.class,graph=train.graph,control=spa.control(gcv="aGCV"))
-
-#Predict with testsets
-
-
-train.class <- label.data[,58]
-label.data <- label.data[,1:54]
-n <- 1:nrow(label.data)
-train.graph1 <- matrix(0,nrow=nrow(label.data),ncol=nrow(label.data))
-
-for(i in n)
+spa.predict<-function(train,test,percentageOfUnlable)
 {
-  for(j in n)
-  {
-    train.graph1[i,j] = sum(apply(rbind(label.data[i,],label.data[j,]),2,min))
-  }
+  sumSet <- nrow(train)+nrow(test)
+  lable_size = sumSet * (1-percentageOfUnlable/100)  
+  #sample labeled data
+  label.row <- sample(1:nrow(train),lable_size)
+  label.data <- train[label.row,]
+  
+  #Unlabeled data
+  unlabel.data <- train[-label.row,]
+  unlabel.data <- rbind(unlabel.data,test)
+  unlabel.data[,58]<-NA
+  
+  
+  #Train Cases
+  mixdata <- rbind(label.data,unlabel.data)
+  mixdata.class <- mixdata[,58]
+  mixdata <- mixdata[,1:30]
+  train.graph <- as.matrix(daisy(mixdata))
+  
+  #SPA Model
+  gsemi<-spa(mixdata.class,graph=train.graph,control=spa.control(gcv="aGCV"))
+  
+  
+  res <- round(gsemi,0)
+  a <- confusion(res, spam[,58])
+  accuracy = 
+  return(a)
+}
+error <- list()
+for(i in 1:100)
+{
+  print(i)
+  error[[i]] <- try(spa.predict(sampleSet,i,800))
+  if(class(error[i]) == "try-error") next;
 }
